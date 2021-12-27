725bfc3eb4717c90db4acada8cd6b886738c8a01
Antho13710 <ryujin13710@gmail.com>
2021-12-27 22:45:25 +0100

    MVP

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,16 +1,40 @@
 <template>
   <Page>
-
+      <ActionBar title="Forza Setup by RyuJin"/>
+      <StackLayout>
+      <ListView for="item in setupTypes" @itemTap="onSetupTypeTap" height="200">
+          <v-template>
+            <Label :text="item.name" textWrap="true" />
+          </v-template>
+      </ListView>
+      <button text="Calculator" @tap="onCalculatorTap" marginTop="50" />
+      </StackLayout>
   </Page>
 </template>
 
 <script lang="ts">
   import Vue from "nativescript-vue";
+ import Calculator from "./Calculator.vue";
+  import SetupData from "../data/SetupData"
+  import SetupDetails from "./SetupDetails.vue"
+
+  const setupData = new SetupData();
 
   export default Vue.extend({
     data() {
         return {
-
+            setupTypes: setupData.getSetupTypes()
+        }
+    },
+    methods: {
+        onSetupTypeTap(args: any)  {
+            const id = args.item.id;
+            this.$navigateTo(SetupDetails, {
+                props: { id }
+            });
+        },
+        onCalculatorTap() {
+          this.$navigateTo(Calculator);
         }
     }
   });
---
+++ app/components/SetupDetails.vue
@@ -0,0 +1,55 @@
+<template>
+  <Page>
+    <ScrollView orientation="vertical">
+      <StackLayout marginLeft="10" marginBottom="20" orientation="vertical">
+        <Label :text="setupType.name" fontSize="24" marginBottom="10" class="text-primary" />
+        <Label text="Pneus" class="text-primary" textWrap="true" />
+        <Label :text="setupType.tyres" class="text-secondary" textWrap="true" />
+        <Label text="Rapports" class="text-primary" textWrap="true" />
+        <Label :text="setupType.gears" class="text-secondary" textWrap="true" />
+        <Label text="Carrossage" class="text-primary" textWrap="true" />
+        <Label :text="setupType.alignment.camber" class="text-secondary" textWrap="true" />
+        <Label text="Parralélisme" class="text-primary" textWrap="true" />
+        <Label :text="setupType.alignment.para" class="text-secondary" textWrap="true" />
+        <Label text="Chasse" class="text-primary" textWrap="true" />
+        <Label :text="setupType.alignment.chasse" class="text-secondary" textWrap="true" />
+        <Label text="Barre Antiroulis" class="text-primary" textWrap="true" />
+        <Label :text="setupType.antiRollBar" class="text-secondary" textWrap="true" />
+        <Label text="Ressort" class="text-primary" textWrap="true" />
+        <Label :text="setupType.springs.springs" class="text-secondary" textWrap="true" />
+        <Label text="Garde au sol" class="text-primary" textWrap="true" />
+        <Label :text="setupType.springs.gas" class="text-secondary" textWrap="true" />
+        <Label text="Rigidité détente" class="text-primary" textWrap="true" />
+        <Label :text="setupType.shockAbsorber.det" class="text-secondary" textWrap="true" />
+        <Label text="Rigidité amortissement" class="text-primary" textWrap="true" />
+        <Label :text="setupType.shockAbsorber.amort" class="text-secondary" textWrap="true" />
+        <Label text="Aérodynamisme" class="text-primary" textWrap="true" />
+        <Label :text="setupType.aeroDynamics" class="text-secondary" textWrap="true" />
+        <Label text="Freins" class="text-primary" textWrap="true" />
+        <Label :text="setupType.brakes" class="text-secondary" textWrap="true" />
+        <Label text="Différentiel avant" class="text-primary" textWrap="true" />
+        <Label :text="setupType.differential.front" class="text-secondary" textWrap="true" />
+        <Label text="Différentiel arrière" class="text-primary" textWrap="true" />
+        <Label :text="setupType.differential.rear" class="text-secondary" textWrap="true" />
+        <Label text="Différentiel central" class="text-primary" textWrap="true" />
+        <Label :text="setupType.differential.center" class="text-secondary" textWrap="true" />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue'
+import SetupData from '~/data/SetupData'
+
+const setupData = new SetupData();
+
+export default Vue.extend({
+  props: ['id'],
+  data() {
+    return {
+      setupType: setupData.getSetupTypeById(this.id)
+    }
+  }
+})
+</script>
